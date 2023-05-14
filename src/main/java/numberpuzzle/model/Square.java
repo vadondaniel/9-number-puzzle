712badd07a5697d16e8ac48b3b7cc5d753a2ec13
Vadon <vadondaniel02@gmail.com>
2023-05-14 11:06:37 +0200

    Added documentation to model/Square.java

--- src/main/java/numberpuzzle/model/Square.java
+++ src/main/java/numberpuzzle/model/Square.java
@@ -1,15 +1,62 @@
 package numberpuzzle.model;
 
+/**
+ * Represents the types of squares that make up the game board.
+ */
 public enum Square {
+
+    /**
+     * Represents an empty square.
+     */
     NONE,
+
+    /**
+     * Represents the circle with the number 1.
+     */
     ONE,
+
+    /**
+     * Represents the circle with the number 2.
+     */
     TWO,
+
+    /**
+     * Represents the circle with the number 3.
+     */
     THREE,
+
+    /**
+     * Represents the circle with the number 4.
+     */
     FOUR,
+
+    /**
+     * Represents the circle with the number 5.
+     */
     FIVE,
+
+    /**
+     * Represents the circle with the number 6.
+     */
     SIX,
+
+    /**
+     * Represents the circle with the number 7.
+     */
     SEVEN,
+
+    /**
+     * Represents the circle with the number 8.
+     */
     EIGHT,
+
+    /**
+     * Represents the circle with the number 9.
+     */
     NINE,
+
+    /**
+     * Represents a square that is on the board but must not be used.
+     */
     BLOCKED
 }
